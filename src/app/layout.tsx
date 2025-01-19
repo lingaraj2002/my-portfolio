2dde72e409314abe08c1cfaee785198a71c55c6c
lingaraj2002 <lingaraj01052002@gmail.com>
2025-01-19 22:19:31 +0530

    commit-19012025-1019

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,29 @@
 import "../styles/globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { Titillium_Web, Poppins } from "next/font/google";
 
 export const metadata = {
   title: "My Portfolio",
   description: "Welcome to my portfolio website",
 };
 
+const titilliumWeb = Titillium_Web({
+  subsets: ["latin"],
+  weight: ["900"],
+  variable: "--font-titillium",
+});
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["300", "400", "600", "700"],
+  variable: "--font-poppins",
+});
+
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <html lang="en">
-      <body>
+      <body className={`${titilliumWeb.variable} ${poppins.variable} `}>
         <Header />
         <main>{children}</main>
         <Footer />
