ebbdd284d9c93f824c2dcbe67c104c8e7d98c093
lingaraj2002 <lingaraj01052002@gmail.com>
2025-08-24 23:30:06 +0530

    commit-24082025-1130

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 /* config options here */
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/my-portfolio",
+  assetPrefix: "/my-portfolio/",
 };
 
 export default nextConfig;
